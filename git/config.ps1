a132a2ebd1565396ea4612506111ac59a6cfea05
Vicente de Alencar <vicentedealencar@gmail.com>
2013-02-07 15:17:31 -0800

    config.ps1 for git

---
+++ git/config.ps1
@@ -0,0 +1,10 @@
+param(
+	[string] $userName = $(Read-Host -prompt "User Name"),
+	[string] $userEmail = $(Read-Host -prompt "User Email")
+)
+
+git config --global user.name $userName
+git config --global user.email $userEmail
+
+git config --global alias.ls "log --pretty=format:'%C(yellow)%h %C(blue)%ad%C(red)%d %C(reset)%s%C(green) [%cn]' --decorate --date=short --graph --max-count=20"
+git config --global alias.st "status -sb"
