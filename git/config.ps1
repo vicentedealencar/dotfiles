022a174fd5b42f86fca09dfb2377fb607cdba877
Vicente de Alencar <vicentedealencar@gmail.com>
2013-02-07 22:16:53 -0200

    adding git graph to config

--- git/config.ps1
+++ git/config.ps1
@@ -8,3 +8,4 @@ git config --global user.email $userEmail
 
 git config --global alias.ls "log --pretty=format:'%C(yellow)%h %C(blue)%ad%C(red)%d %C(reset)%s%C(green) [%cn]' --decorate --date=short --graph --max-count=20"
 git config --global alias.st "status -sb"
+git config --global alias.graph "log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
