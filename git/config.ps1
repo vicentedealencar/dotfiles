5c485a81393f090e52fdf075a539ded7a6b305fe
Vicente de Alencar <vicentedealencar@gmail.com>
2013-02-14 14:49:54 -0200

    git st to git s

--- git/config.ps1
+++ git/config.ps1
@@ -7,5 +7,5 @@ git config --global user.name $userName
 git config --global user.email $userEmail
 
 git config --global alias.l "log --pretty=format:'%C(yellow)%h %C(blue)%ad%C(red)%d %C(reset)%s%C(green) [%cn]' --decorate --date=short --graph --max-count=20"
-git config --global alias.st "status -sb"
+git config --global alias.s "status -sb"
 git config --global alias.graph "log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
