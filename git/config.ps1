7c0e4f39e032454e538477bed026e77cd813c175
Vicente de Alencar <vicentedealencar@gmail.com>
2013-02-17 19:32:29 -0300

    adding pusblish heroku and azure

--- git/config.ps1
+++ git/config.ps1
@@ -9,3 +9,5 @@ git config --global user.email $userEmail
 git config --global alias.l "log --pretty=format:'%C(yellow)%h %C(blue)%ad%C(red)%d %C(reset)%s%C(green) [%cn]' --decorate --date=short --graph --max-count=20"
 git config --global alias.s "status -sb"
 git config --global alias.graph "log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
+git config --global alias.publishazure "push azure master"
+git config --global alias.publishheroku "push heroku master"
