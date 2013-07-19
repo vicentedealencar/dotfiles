4760865935f5dd9a3c84a8c822cdefb2a79eb65a
Vicente de Alencar <vicentedealencar@gmail.com>
2013-07-19 02:13:59 -0300

    removing name and email config

--- git/aliases.ps1
+++ git/aliases.ps1
@@ -1,11 +1,3 @@
-param(
-	[string] $userName = $(Read-Host -prompt "User Name"),
-	[string] $userEmail = $(Read-Host -prompt "User Email")
-)
-
-git config --global user.name $userName
-git config --global user.email $userEmail
-
 git config --global alias.l "log --pretty=format:'%C(yellow)%h %C(blue)%ad%C(red)%d %C(reset)%s%C(green) [%cn]' --decorate --date=short --graph --max-count=20"
 git config --global alias.s "status -sb"
 git config --global alias.graph "log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
