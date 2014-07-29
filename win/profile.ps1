cacf8b4adb7eb7acdcbe40a0945d9a465947cf6f
Vicente de Alencar <vicentedealencar@gmail.com>
2014-07-29 14:46:23 -0300

    Set-Alias exists

--- win/profile.ps1
+++ win/profile.ps1
@@ -5,20 +5,21 @@
 # Load posh-npm example profile
 . 'C:\Users\Vicente\Documents\WindowsPowerShell\Modules\posh-npm\profile.example.ps1'
 
-function subl { . 'C:\Program Files\Sublime Text 3\sublime_text.exe' $args }
+Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
 
-function nuget { . 'C:\Users\Vicente\Downloads\NuGet.exe' $args }
+Set-Alias nuget 'C:\Users\Vicente\Downloads\NuGet.exe'
 
-function scriptcs { . 'C:\Users\Vicente\Documents\GitHub\scriptcs\src\Scriptcs\bin\Release\Scriptcs.exe' $args }
+Set-Alias scriptcs 'C:\Users\Vicente\Documents\GitHub\scriptcs\src\Scriptcs\bin\Release\Scriptcs.exe'
 
-function elasticsearch { . 'C:\bin\elasticsearch-0.90.5-vanilla\elasticsearch-0.90.5\bin\elasticsearch.bat' $args }
+Set-Alias elasticsearch 'C:\bin\elasticsearch-0.90.5-vanilla\elasticsearch-0.90.5\bin\elasticsearch.bat'
 
-function CHROME_BIN { . 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' $args }
+Set-Alias CHROME_BIN 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+
+Set-Alias gut git
 
 function ss {
-    foreach($file in ls)
-    {
-        if($file.Name.Contains('.sln')){
+    foreach($file in ls) {
+        if($file.Name.Contains('.sln')) {
             Write-Host 'Starting $file'
             start $file
         }
@@ -29,11 +30,11 @@ function gut() { . 'git' $args }
 
 function gs() { . 'git' 'status'$args }
 
-function bring-branch ($branch) {
+function git-bring-branch ($branch) {
     . 'git' 'checkout' $branch
     . 'git' 'pull'
     . 'git' 'checkout' '-'
     . 'git' 'merge' $branch
 }
 
-echo 'profile loaded'
+Write-Host 'profile loaded' -ForegroundColor yellow
