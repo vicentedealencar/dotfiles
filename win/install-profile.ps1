b9d50f2eec1183febfe5ea7233b6d203482a47e6
Vicente de Alencar <vicentedealencar@gmail.com>
2015-01-06 04:48:26 -0200

    consolez configuration

--- win/install-profile.ps1
+++ win/install-profile.ps1
@@ -1,7 +1,11 @@
 $profilePath = "$PSScriptRoot\profile.ps1"
+echo ". '$profilePath'" >> $PROFILE
 
-echo "
 
-. '$profilePath'
 
-" >> $PROFILE
+if(-not(Test-Path ~\AppData\Roaming\Console)){
+  New-Item ~\AppData\Roaming\Console -type directory | Out-Null
+}
+
+$consolePath = "$PSScriptRoot\console.xml"
+echo ". '$consolePath'" >> ~\AppData\Roaming\Console\console.xml
